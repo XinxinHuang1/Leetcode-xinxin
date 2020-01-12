9ac90775367d9be8dcea910b783a80ca7d5ab4e6
Xinxin <huang.xinx@husky.neu.edu>
2020-01-12 14:42:02 -0500

    dp

---
+++ Algorithms/63.Unique Paths II.java
@@ -0,0 +1,31 @@
+
+//dynamic programming
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length, col = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1)
+            return 0;
+        obstacleGrid[0][0] = 1;
+        for(int i = 1; i < row; i++){
+            if(obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1)
+                obstacleGrid[i][0] = 1;
+            else
+                obstacleGrid[i][0] = 0;
+        }
+        for(int j = 1; j < col; j++){
+            if(obstacleGrid[0][j] == 0 && obstacleGrid[0][j-1] == 1)
+                obstacleGrid[0][j] = 1;
+            else
+                obstacleGrid[0][j] = 0;
+        }
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(obstacleGrid[i][j] == 0)
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                else 
+                    obstacleGrid[i][j] = 0;
+            }
+        }
+        return obstacleGrid[row-1][col-1];
+    }
+}
