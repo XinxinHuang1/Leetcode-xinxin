7ce7a9026990c79582b58d43e6948931444b5726
Xinxin <huang.xinx@husky.neu.edu>
2020-01-17 13:15:10 -0500

    integer to words' :

---
+++ Algorithms/273.Integer to Engilsh Words.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ * Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+
+Example 3:
+
+Input: 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+Example 4:
+
+Input: 1234567891
+Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+ */
+
+class Solution {
+    private final String[] less_than_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",   "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] thousands = {"", "Thousand", "Million", "Billion"};
+    
+    public String numberToWords(int num) {
+        if(num == 0)
+            return "Zero";
+        
+        int i = 0;
+        String ans = "";
+        while(num > 0){
+            if(num % 1000 != 0){
+                ans = helper(num % 1000) + thousands[i] + " " + ans;
+            }
+            num /= 1000;
+            i++;
+        }
+        return ans.trim();
+    }
+    
+    private String helper(int num){
+        if(num == 0) 
+            return "";
+        else if(num < 20)
+            return less_than_20[num] + " ";
+        else if(num < 100)
+            return tens[num / 10] + " " + helper(num % 10);
+        else
+            return less_than_20[num / 100] + " Hundred " + helper(num % 100);
+    }
+    
+}
