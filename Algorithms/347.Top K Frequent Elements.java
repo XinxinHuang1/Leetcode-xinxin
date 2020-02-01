e9d15626a8ce40a4663f41f0c1fd7a886d307e93
Xinxin <huang.xinx@husky.neu.edu>
2020-02-01 00:16:17 -0500

    O(N)

--- Algorithms/347.Top K Frequent Elements.java
+++ Algorithms/347.Top K Frequent Elements.java
@@ -36,3 +36,30 @@ class Solution {
         return res;
     }
 }
+
+//Solution 2 : Time: O(N)
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        for(int i = bucket.length - 1; i >= 0 && res.size() < k; i--){
+            if(bucket[i] != null)
+                res.addAll(bucket[i]);
+        }
+        return res;
+    }
+}
