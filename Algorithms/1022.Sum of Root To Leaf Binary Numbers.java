6147c1e8dabbcfc96dfbf5be59771fd69896f306
Xinxin <huang.xinx@husky.neu.edu>
2020-02-24 16:24:38 -0500

    tree dfs

---
+++ Algorithms/1022.Sum of Root To Leaf Binary Numbers.java
@@ -0,0 +1,39 @@
+/*
+Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+Example 1:
+
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    public int dfs(TreeNode root, int sum){
+        if(root == null)
+            return 0;
+        
+        int cur = root.val + sum;
+        if(root.left == null && root.right == null){
+            return cur;
+        }
+        return dfs(root.left, cur * 2) + dfs(root.right, cur * 2);
+    }
+}
