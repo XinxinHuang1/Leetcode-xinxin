ccffe724d6fa3eda0282a0979284324371ff0f97
Xinxin <huang.xinx@husky.neu.edu>
2020-03-05 13:58:41 -0500

    queue

---
+++ Algorithms/482.License Key Formatting.java
@@ -0,0 +1,64 @@
+
+/*
+You are given a license key represented as a string S which consists only alphanumeric character and dashes. The string is separated into N+1 groups by N dashes.
+
+Given a number K, we would want to reformat the strings such that each group contains exactly K characters, except for the first group which could be shorter than K, but still must contain at least one character. Furthermore, there must be a dash inserted between two groups and all lowercase letters should be converted to uppercase.
+
+Given a non-empty string S and a number K, format the string according to the rules described above.
+
+Example 1:
+Input: S = "5F3Z-2e-9-w", K = 4
+
+Output: "5F3Z-2E9W"
+
+Explanation: The string S has been split into two parts, each part has 4 characters.
+Note that the two extra dashes are not needed and can be removed.
+Example 2:
+Input: S = "2-5g-3-J", K = 2
+
+Output: "2-5G-3J"
+
+Explanation: The string S has been split into three parts, each part has 2 characters except the first part as it could be shorter as mentioned above. */
+
+
+
+class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+        Queue<Character> queue = new LinkedList<Character>();
+        for(char c : S.toCharArray()){
+            if(c != '-')
+                queue.add(Character.toUpperCase(c));
+        }
+        int first = queue.size() % K;
+        ArrayList<Character> chars = new ArrayList<>();
+        if(queue.size() % K > 0){
+            while(first > 0){
+                chars.add(queue.poll());
+                first--;
+            }
+            chars.add('-');
+        }
+        int len = K;
+        while(!queue.isEmpty()){
+            while(len > 0){
+                chars.add(queue.poll());
+                len--;
+            }
+            chars.add('-');
+            len = K;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < chars.size() - 1; i++){
+            sb.append(chars.get(i));
+        }
+        return sb.toString();
+    }
+}
+
+/*
+2 5 A 3 J  - K = 2
+2 5A 3J
+
+12 345678 K = 3
+12-345-
+*/
