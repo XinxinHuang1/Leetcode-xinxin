970313be339e326cedf10b9a0c1dc737ced4e802
Xinxin <huang.xinx@husky.neu.edu>
2020-03-04 22:12:22 -0500

    toUpperCase String

---
+++ Algorithms/520.Detect Capital.java
@@ -0,0 +1,36 @@
+/*
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+ */
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() < 2)
+            return true;
+        if(word.equals(word.toUpperCase()))
+            return true;
+        else if(word.equals(word.toLowerCase()))
+            return true;
+        else if(Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase()))
+            return true;
+        return false;
+    }
+}
