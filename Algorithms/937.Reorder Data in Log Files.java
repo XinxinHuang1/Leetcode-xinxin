f237a92b3573cf59debc3f88bdcabc6f8a0b023c
Xinxin <huang.xinx@husky.neu.edu>
2020-04-24 15:38:34 -0700

    string compareTo

---
+++ Algorithms/937.Reorder Data in Log Files.java
@@ -0,0 +1,71 @@
+/*
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+*/
+
+//Time Complexity: O(AlogA), where \mathcal{A}A is the total content of logs.
+
+//Space Complexity: O(A). 
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (log1, log2) ->{
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+            if(!isDigit1 && !isDigit2){
+                int compare = split1[1].compareTo(split2[1]);
+                if(compare != 0)
+                    return compare;
+                return split1[0].compareTo(split2[0]);
+            }
+            
+            //-1, 0, 1
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
+    }
+}
+
+//comparator
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Comparator<String> comp = new Comparator<>(){
+            @Override
+            public int compare(String s1, String s2){
+                String[] split1 = s1.split(" ", 2);
+                String[] split2 = s2.split(" ", 2);
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+                if(!isDigit1 && !isDigit2){
+                    int res = split1[1].compareTo(split2[1]);
+                    if(res != 0)
+                        return res;
+                    else
+                        return split1[0].compareTo(split2[0]);
+                }
+                else
+                    return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+            }
+        };
+        Arrays.sort(logs, comp);
+        return logs;
+    }
+}
