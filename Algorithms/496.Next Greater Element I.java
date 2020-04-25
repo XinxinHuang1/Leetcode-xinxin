b86c9f6bc6b079822cd31e333e305c2a3dd7abe6
Xinxin <huang.xinx@husky.neu.edu>
2020-04-25 00:24:47 -0700

    stack

--- Algorithms/496.Next Greater Element I.java
+++ Algorithms/496.Next Greater Element I.java
@@ -5,7 +5,7 @@ The Next Greater Number of a number x in nums1 is the first greater number to it
 
 Example 1:
 Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
-Output: [-1,3,-1] */
+Output: [-1,3,-1] **/
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
