95712424077354f5847245055665a7dde6f1419d
Xinxin <huang.xinx@husky.neu.edu>
2020-04-25 13:04:35 -0700

    tree dfs

---
+++ Algorithms/938.Range Sum of BST.java
@@ -0,0 +1,44 @@
+/*
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+ 
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int sum;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        dfs(root, L, R);
+        return sum;
+    }
+    
+    private void dfs(TreeNode root, int L, int R){
+        if(root == null)
+            return;
+        if(root.val >= L && root.val <= R)
+            sum += root.val;
+
+        //use feature of BST to improve efficiency
+        if(root.val > L)
+            dfs(root.left, L, R);
+        if(root.val < R)
+            dfs(root.right, L, R);
+    }
+}
